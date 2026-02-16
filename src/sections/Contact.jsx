5b066aebb56618d16732be6f04d176e9b271422e
Mwas.Dev <96233246+Simon749@users.noreply.github.com>
2026-02-16 08:47:19 +0000

    update contact no.

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -53,7 +53,7 @@ const Contact = () => {
               <h2>Phone</h2>
               <div className="w-full h-px my-2 bg-white/30" />
               <p className="text-xl lowercase md:text-2xl lg:text-3xl">
-                +254 729047823
+                +254 756137400
               </p>
             </div>
             <div className="social-link">
